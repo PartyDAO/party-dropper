093650ffe428a92d4abca7790509065c6a7d3137
Steve Klebanoff <steve.klebanoff@gmail.com>
2021-12-21 21:25:12 -0800

    events

--- src/PartyDropper.sol
+++ src/PartyDropper.sol
@@ -17,9 +17,18 @@ contract PartyDropper is ERC1155, Ownable {
         string description;
     }
     mapping(uint256 => Edition) public editions;
-    mapping(uint256 => mapping(address => bool)) public editionMinters;
+    mapping(uint256 => mapping(address => bool)) public editionMinter;
+    mapping(uint256 => address) public editionCreator;
     mapping(address => bool) public allowedCreators;
     uint256 lastEditionId;
+
+    event EditionCreated(
+        uint256 indexed editionId,
+        address indexed creator,
+        address party,
+        string name
+    );
+    event Claimed(address indexed user, uint256 editionId);
 
     modifier onlyAllowedCreator() {
         require(allowedCreators[msg.sender], "must be allowed creator");
@@ -35,9 +44,24 @@ contract PartyDropper is ERC1155, Ownable {
             fnd.party.totalContributed(msg.sender) > 0,
             "didn't contribute to PartyBid"
         );
-        require(!editionMinters[editionId][msg.sender], "already minted");
-        editionMinters[editionId][msg.sender] = true;
+        require(!editionMinter[editionId][msg.sender], "already minted");
+        editionMinter[editionId][msg.sender] = true;
         _mint(msg.sender, editionId, 1, "");
+        emit Claimed(msg.sender, editionId);
+    }
+
+    function editionInfo(uint256 editionId)
+        public
+        view
+        returns (
+            address,
+            string memory,
+            string memory,
+            string memory
+        )
+    {
+        Edition storage fnd = editions[editionId];
+        return (address(fnd.party), fnd.name, fnd.imageURI, fnd.description);
     }
 
     function uri(uint256 editionId)
@@ -83,6 +107,8 @@ contract PartyDropper is ERC1155, Ownable {
             imageURI: _imageURI,
             description: _description
         });
+        editionCreator[lastEditionId] = msg.sender;
+        emit EditionCreated(lastEditionId, msg.sender, address(_party), _name);
     }
 
     // owner functions
